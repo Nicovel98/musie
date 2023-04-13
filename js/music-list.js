d7473067654fd8df569e0bb44ad61d21b294f63c
Nicovel98 <nicovel98@gmail.com>
2023-04-13 17:08:46 -0500

    Primera versión del reproductor

---
+++ js/music-list.js
@@ -0,0 +1,92 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [{
+        name: "Transistor - In Circles",
+        artist: "Red",
+        img: "Transistor.jpg",
+        src: "Transistor-In-Circles"
+    },
+    {
+        name: "Transistor - Signals",
+        artist: "Red",
+        img: "Transistor.jpg",
+        src: "Transistor-Signals"
+    },
+    {
+        name: "Transistor - The Spine",
+        artist: "Red",
+        img: "Transistor.jpg",
+        src: "Transistor-The-Spine"
+    },
+    {
+        name: "Transistor - She Shines",
+        artist: "Red",
+        img: "Transistor-1.gif",
+        src: "Transistor-She-Shines"
+    },
+    {
+        name: "Transistor - Paper Boats",
+        artist: "Red",
+        img: "Transistor.jpg",
+        src: "Transistor-Paper-Boats"
+    },
+    {
+        name: "Transistor - We All Become",
+        artist: "Red",
+        img: "Transistor.jpg",
+        src: "Transistor-We-All-Become"
+    },
+    /* {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-1",
+        src: "music-1"
+    },
+
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "music-6"
+    }, */
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+];
